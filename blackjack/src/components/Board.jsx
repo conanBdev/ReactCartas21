393792e3f94a2189eda9d9641fa8c807144b19e1
AngelaDev <angelaprogweb@gmail.com>
2024-06-16 13:48:07 +0200

    Revert "Revert "NEW - Creacion de menu de juego, estilos css basico""
    This reverts commit e9a101b779cffe5aa8cc3cbf9a47253d74d237bd.

---
+++ blackjack/src/components/Board.jsx
@@ -0,0 +1,15 @@
+
+export default function Board() {
+    return(
+        <>
+        <section className="bj-board">            
+            <div className="bj-board-crupier-card">
+                
+            </div>
+            <div className="bj-board-player-card">
+
+            </div>
+        </section>
+        </>
+    )
+}
